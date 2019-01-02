0ae2acc4f28490ce84fb75fde17403d886567f27
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2019-01-02 23:50:24 +0000

    adding 2 challenge specs

---
+++ 2.js
@@ -0,0 +1,6 @@
+  //2. Given a string of words (up to 2,000 let’s say), write a function which returns the following information as JSON 
+  // ● Total number of words 
+  // ● Total number of sentences 
+  // ● Longest word 
+  // ● Average length of word used 
+  // ● Number of words above 3 characters 
