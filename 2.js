5a4386ed7981347a45df2f21c453dd88351c5cf3
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2019-01-03 02:07:35 +0000

    building out initial functions and JSON structure

--- 2.js
+++ 2.js
@@ -1,6 +1,33 @@
-  //2. Given a string of words (up to 2,000 let’s say), write a function which returns the following information as JSON 
+  //2. Given a string of words (up to 2,000 let’s say), write a function which returns the following 
+  // information as JSON 
   // ● Total number of words 
   // ● Total number of sentences 
   // ● Longest word 
   // ● Average length of word used 
   // ● Number of words above 3 characters 
+
+function toJson {
+
+  function numWords(str){
+    return str.split(' ').length;
+  }
+
+  console.log(numWords('hello my name is Sophie'))
+
+  function numSentences(str){
+    return str.split(".").length;
+  }
+
+  console.log(numSentences(""))
+
+}
+  // need to return all results as object pairs
+
+  //
+  "results":[
+  {"numWords": int},
+  {"numSentences": int},
+  {"longestWord": "string"},
+  {"avWordLength": int},
+  {"numWordsOverthree": int}
+  ]
